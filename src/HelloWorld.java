6e81e48905aafeed4a6d4486d843a46ac20de681
charith28 <37987551+charith28@users.noreply.github.com>
2021-09-01 11:08:45 -0400

    Ok

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args){
+        int num1 = 39, num2 = 39;
+        System.out.println(num1+num2);
+        for(String word : args){
+            System.out.println(word);
+        }
 
-        System.out.println();
     }
 }
