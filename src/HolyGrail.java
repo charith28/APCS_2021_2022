d8c0edfd14da6d1a036ed2f19454740d5e0b9649
23fernando <charith28@gmail.com>
2021-09-13 08:42:06 -0400

    Testing Testing 123

--- src/HolyGrail.java
+++ src/HolyGrail.java
@@ -1,2 +1,10 @@
+/**
+ * Class HolyGrail is a simple dialogue code
+ * @version 09/15/2021
+ * @author 23fernando
+ */
 public class HolyGrail {
+    public static void main(String[] args) {
+        System.out.println("Testing push");
+    }
 }
