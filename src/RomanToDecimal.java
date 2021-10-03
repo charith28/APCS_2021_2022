7c9ca731aa94a673445a3065560ac07b2dae5886
23fernando <charith28@gmail.com>
2021-10-03 11:24:03 -0400

    Final Form

--- src/RomanToDecimal.java
+++ src/RomanToDecimal.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
+/**
+ * RomanToDecimal is a program which converts both user and args input of Roman Numerals into Integer form, while handling invalid and incorrect input.
+ * @version Sunday October 3, 2021
+ * @author 23fernando
+ */
 public class RomanToDecimal {
+    /**
+     * Takes parameter roman and uses a for loop to determine the number values of each character. It then checks for invariants and subtracts the corresponding amount.
+     * The .substring, .toUpperCase, .length, and .indexOf methods are implemented in this method.
+     * @param roman - User or args input of a roman numeral. This parameter is parsed through and analyzed for its characters which determine the integer equivalent.
+     * @return - Program returns -1 if invalid or the integer format of the Roman Numeral if input is valid
+     */
     public static int romanToDecimal(String roman){
         int decimal = 0;
+
         for(int i = 0;i < roman.length(); i++){
             //System.out.print(roman.substring(i,i+1).toUpperCase());
             String upperChar = roman.substring(i, i + 1).toUpperCase();
@@ -41,10 +53,17 @@ public class RomanToDecimal {
             decimal -= 20;
         if(roman.toUpperCase().indexOf("CD") != -1)
             decimal -=200;
-        if(roman.toUpperCase().indexOf("CM") != -1)
+        if(roman.toUpperCase().indexOf("CM") != -1) {
             decimal -= 200;
-        return decimal;
+        }
+       return isLogVal(roman.toUpperCase(), decimal);
     }
+
+    /**
+     * Uses a subtractive for loop and while loop which takes away the largest possible number from value and then adds the corresponding letter(s) to the end of an empty string.
+     * @param value - A integer value which is converted into a Roman numeral
+     * @return - This method returns a String which is the correct Roman numeral for any value between 1-10000.
+     */
     public static String decimalToRoman(int value){
         String logVal = "";
         String[] rom = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
@@ -56,14 +75,28 @@ public class RomanToDecimal {
 
             }
         }
+
         return logVal;
     }
 
-    public static boolean isLogVal(String numeral, int value) {
-        return decimalToRoman(value) == numeral;
+    /**
+     * A simple if else statement that checks the equivalency of the user input and the correct roman numeral for a given value.
+     * This method is run at the end of decimalToRoman, to filter for invalid roman numerals that have been converted.
+     * @param numeral - The user or args input of a roman numeral is put here.
+     * @param value - The value calculated by the decimalToRoman method is put here.
+     * @return - The method returns -1 if the user/args input is not correct or valid, otherwise returns the value of the conversion.
+     */
+    public static int isLogVal(String numeral, int value) {
+        if (numeral.toUpperCase().equals(decimalToRoman(value))){
+            return value;
+        }
+        else{ return -1;}
     }
 
-
+    /**
+     * Main entry point. Runs the args test set and then handles user inputs.
+     * @param args - contain a test set Roman Numerals and invalid inputs.
+     */
     public static void main(String[] args) {
         Scanner user =  new Scanner(System.in);
         RomanToDecimal obj = new RomanToDecimal();
@@ -73,12 +106,18 @@ public class RomanToDecimal {
             if(val == -1){
                 System.out.println("Input: " + temp + " => output: invalid");
             }
-            else if (!isLogVal(temp.toUpperCase(),val)){
-                System.out.println("Input: " + temp + " => output: invalid form");
-            }
             else{
                 System.out.println("Input: " + temp + " => output: " + val);
             }
+        }
+        System.out.print("Input: ");
+        String uinp = user.nextLine();
+        int udec = romanToDecimal(uinp);
+        if(udec == -1){
+            System.out.print("=> output: invalid");
+        }
+        else{
+            System.out.print("=> output: " + udec);
         }
     }
 }
--- src/testRomanToDecimalTest.java
+++ src/testRomanToDecimalTest.java
@@ -2,10 +2,27 @@ import static org.junit.Assert.*;
 public class testRomanToDecimalTest {
 
     @org.junit.Test
-    public void romanToDeciaml() {
+    public void romanToDecimal() {
         assertEquals(RomanToDecimal.romanToDecimal("XI"),11);
-        assertEquals(RomanToDecimal.romanToDecimal("IXIX"),20);
+        assertEquals(RomanToDecimal.romanToDecimal("xx"),20);
+        assertEquals(RomanToDecimal.romanToDecimal("Xx"),20);
+        assertEquals(RomanToDecimal.romanToDecimal("CMLXXIX"),979);
+        assertEquals(RomanToDecimal.romanToDecimal("DCCXXV"),725);
+        assertEquals(RomanToDecimal.romanToDecimal("DXXXVI"),536);
+        assertEquals(RomanToDecimal.romanToDecimal("CCXXVI"),226);
+        assertEquals(RomanToDecimal.romanToDecimal("DCCLXIV"),764);
+        assertEquals(RomanToDecimal.romanToDecimal("CCCXXXIX"),339);
+        assertEquals(RomanToDecimal.romanToDecimal("CCCLXXVII"),377);
+        assertEquals(RomanToDecimal.romanToDecimal("IXIX"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("IVDDCCXXX"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("XVIICMX"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("IIV"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("MRCOCHRAN"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("XCISIS"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("COMPSCIISCOOOL"),-1);
+        assertEquals(RomanToDecimal.romanToDecimal("TAKETHIS-JAVA"),-1);
         assertNotEquals(RomanToDecimal.romanToDecimal("Joe"),20);
+        assertNotEquals(RomanToDecimal.romanToDecimal("IC"),101);
 
     }
 }
