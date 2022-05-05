35918eb75936946fb02825f7762030db608a3cd0
23fernando <charith28@gmail.com>
2022-05-05 09:15:22 -0400

    1/20/22

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,2 +1,74 @@
+import java.util.ArrayList;
+
 public class MergeSort {
+    ArrayList<Waypoint> input;
+    WaypointComparator wayCo;
+
+    public MergeSort(ArrayList<Waypoint> input, WaypointComparator wc) {
+        this.input = input;
+        wayCo = wc;
+    }
+
+    public void mergeSort(ArrayList<Waypoint> in){
+        int debug = 0;
+        if(in.size()<2){
+            return;
+        }
+        int middle = in.size()/2;
+        ArrayList<Waypoint> left = new ArrayList<Waypoint>();
+        ArrayList<Waypoint> right = new ArrayList<Waypoint>();
+        for(Waypoint w: in){
+            if(in.indexOf(w) < middle)
+                left.add(w);
+            else
+                right.add(w);
+        }
+        //System.out.println(left.size());
+        //System.out.println(right.size());
+        mergeSort(left);
+        System.out.println("debug"+debug);
+        mergeSort(right);
+        merge(left,right,wayCo);
+
+    }
+
+    public void merge(ArrayList<Waypoint> left, ArrayList<Waypoint> right, WaypointComparator wc){
+        int leftSize= left.size();
+        int rightSize = right.size();
+        int i = 0, j= 0, k=0;
+        while(i<leftSize && j< rightSize){
+            if (wc.compare(left.get(i),right.get(j))<0){
+                input.set(k,left.get(i));
+                i++;
+            }
+            else{
+                input.set(k,right.get(k));
+                j++;
+            }
+            k++;
+        }
+        while(i<leftSize){
+            input.set(k,left.get(i));
+            i++;
+            k++;
+        }
+        while(j<rightSize){
+            input.set(k, left.get(j));
+            j++;
+            k++;
+        }
+
+    }
+
+    public void printDatabase() {
+        for (Waypoint w : input)
+            System.out.println(w);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 }
+
+
--- src/TrailDatabase.java
+++ src/TrailDatabase.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.CDATASection;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,7 +15,7 @@ public class TrailDatabase {
     }
     public void populateDatabase(){
         try{
-            Scanner in = new Scanner(new File("datafiles/apptrailDB.txt"));
+            Scanner in = new Scanner(new File("apptrailDB.txt"));
             while(in.hasNext()){
                 String[] data = in.nextLine().split("\t");
                 database.add(new Waypoint(data[0], data[1], data[2], Double.parseDouble(data[5]), Double.parseDouble(data[6]), Integer.parseInt(data[7])));
@@ -25,22 +27,34 @@ public class TrailDatabase {
         }
     }
 
-    public void printDatabase(){
 
+    public void getSearchTerms(){
+        System.out.println("*** Welcome to the Appalachian Trail Database ***\n \t+ Menu of search terms to use for sorting waypoints +\n \tTY: by name\n \tNA: by name\n \tST: by state\n \tDS: by distance to Springer\n \tDK: by distance to Katahdin\n \tEL: by elevation");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your preferred sort by term or 'Q' to quit: ");
+        String term = in.nextLine();
+        //
     }
 
-    public void printDB() {
+
+
+
+    public void printDatabase() {
         for (Waypoint w : database)
             System.out.println(w);
     }
 
-
-
     public void sortDB(WaypointComparator wc){
-        Collections.sort(database, new WaypointComparator(6,false));
+        Collections.sort(database, wc);
     }
 
     public static void main(String[] args) {
+        TrailDatabase t1 = new TrailDatabase();
+        MergeSort m1 = new MergeSort(t1.database, new WaypointComparator(5,false));
+        t1.sortDB(new WaypointComparator(5,false));
+        t1.printDatabase();
+        m1.mergeSort(t1.database);
+        m1.printDatabase();
 
     }
 }
