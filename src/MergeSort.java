3aab31b4a5ff45bb1756d8a6245afac007fa61a6
23fernando <charith28@gmail.com>
2022-05-06 17:33:16 -0400

    1/20/22

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,66 +3,79 @@ import java.util.ArrayList;
 public class MergeSort {
     ArrayList<Waypoint> input;
     WaypointComparator wayCo;
+    ArrayList<Waypoint> output = new ArrayList<>();
 
-    public MergeSort(ArrayList<Waypoint> input, WaypointComparator wc) {
-        this.input = input;
+    public MergeSort(ArrayList<Waypoint> in, WaypointComparator wc) {
+        input = in;
         wayCo = wc;
     }
 
     public void mergeSort(ArrayList<Waypoint> in){
-        int debug = 0;
-        if(in.size()<2){
+        int count = 0;
+
+        if(in.size() == 1){
             return;
         }
         int middle = in.size()/2;
         ArrayList<Waypoint> left = new ArrayList<Waypoint>();
         ArrayList<Waypoint> right = new ArrayList<Waypoint>();
-        for(Waypoint w: in){
-            if(in.indexOf(w) < middle)
-                left.add(w);
-            else
-                right.add(w);
+        for(int i = 0; i< middle;i++)
+            left.add(in.get(i));
+        for(int l = middle; l<in.size();l++){
+            right.add(in.get(l));
         }
-        //System.out.println(left.size());
-        //System.out.println(right.size());
+        System.out.println(count);
+        System.out.println("LEFT");
+        for (Waypoint w:left) {
+            System.out.println(w);
+        }
+        System.out.println("RIGHT");
+        for (Waypoint w:right) {
+            System.out.println(w);
+        }
         mergeSort(left);
-        System.out.println("debug"+debug);
         mergeSort(right);
-        merge(left,right,wayCo);
+        merge(left,right);
 
+            System.out.println("Begin Iteration");
+            printDatabase();
+            System.out.println("End Iteration");
     }
 
-    public void merge(ArrayList<Waypoint> left, ArrayList<Waypoint> right, WaypointComparator wc){
+    public void merge(ArrayList<Waypoint> left, ArrayList<Waypoint> right){
         int leftSize= left.size();
         int rightSize = right.size();
-        int i = 0, j= 0, k=0;
-        while(i<leftSize && j< rightSize){
-            if (wc.compare(left.get(i),right.get(j))<0){
-                input.set(k,left.get(i));
-                i++;
+        int l = 0, r= 0, k=0;
+        while(l<leftSize && r< rightSize){
+            if (wayCo.compare(left.get(l),right.get(r)) < 0){ // if left is less than right
+                System.out.println(left.get(l).getElevation() + " greater than " + right.get(r).getElevation());
+                output.set(k++,left.get(l++));
             }
             else{
-                input.set(k,right.get(k));
-                j++;
+                System.out.println(left.get(l).getElevation() + " less than " + right.get(r).getElevation());
+                output.set(k++,right.get(r++));
             }
-            k++;
+
         }
-        while(i<leftSize){
-            input.set(k,left.get(i));
-            i++;
-            k++;
+        while(l<leftSize){
+            output.set(k++, left.get(l++));
+
         }
-        while(j<rightSize){
-            input.set(k, left.get(j));
-            j++;
-            k++;
+        while(r<rightSize){
+            output.set(k++, right.get(r++));
+
         }
 
     }
 
     public void printDatabase() {
-        for (Waypoint w : input)
+        for (Waypoint w : output)
             System.out.println(w);
+    }
+    public void nullArray(){
+        for(Waypoint w: input){
+            output.add(new Waypoint());
+        }
     }
 
     public static void main(String[] args) {
